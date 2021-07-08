ca32dac559455e6acbd93d4d508b8dee832ce57f
alfmengra1990 <71791219+alfmengra1990@users.noreply.github.com>
2021-07-07 20:39:57 -0600

    Inicio de Login Usuarios
    Clase Util con la encriptación y desencriptación.
    Las interfaces y los repositorios.
    Inicio de Login Usuarios

---
+++ ApplicationCore/Services/ServiceUsuario.cs
@@ -0,0 +1,39 @@
+﻿using ApplicationCore.Utils;
+using Infraestructure.Models;
+using Infraestructure.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Services
+{
+    public class ServiceUsuario : IServiceUsuario
+    {
+        public Usuario GetUsuarioByID(int id)
+        {
+            IRepositoryUsuario repository = new RepositoryUsuario();
+            Usuario oUsuario = repository.GetUsuarioByID(id);
+            oUsuario.Contra = Cryptography.DecrypthAES(oUsuario.Contra);
+            return oUsuario;
+        }
+
+        public Usuario Save(Usuario usuario)
+        {
+            IRepositoryUsuario repository = new RepositoryUsuario();
+            return repository.Save(usuario);
+        }
+
+        public Usuario GetUsuario(string email, string password)
+        {
+            IRepositoryUsuario repository = new RepositoryUsuario();
+
+            //Encriptar la contraseña para poder compararlo
+            string crytpPasswd = Cryptography.DecrypthAES(password);
+            return repository.GetUsuario(email, crytpPasswd);
+        }
+
+
+    }
+}
---
+++ Infraestructure/Repository/RepositoryUsuario.cs
@@ -0,0 +1,127 @@
+﻿using Infraestructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructure.Repository
+{
+    public class RepositoryUsuario : IRepositoryUsuario
+    {
+        public Usuario GetUsuarioByID(int id)
+        {
+            Usuario usuario = null;
+            try
+            {
+                using (MyContext ctx = new MyContext())
+                {
+                    ctx.Configuration.LazyLoadingEnabled = false;
+                    usuario = ctx.Usuario.
+                        Include("Rol").
+                        Where(p => p.IdUsuario == id).FirstOrDefault<Usuario>();
+                }
+                return usuario;
+            }
+            catch (DbUpdateException dbEx)
+            {
+                string mensaje = "";
+                Log.Error(dbEx, System.Reflection.MethodBase.GetCurrentMethod(), ref mensaje);
+                throw new Exception(mensaje);
+            }
+            catch (Exception ex)
+            {
+                string mensaje = "";
+                Log.Error(ex, System.Reflection.MethodBase.GetCurrentMethod(), ref mensaje);
+                throw;
+            }
+        }
+
+        public Usuario Save(Usuario usuario)
+        {
+            int retorno = 0;
+            Usuario oUsuario = null;
+            try
+            {
+                using (MyContext ctx = new MyContext())
+                {
+                    ctx.Configuration.LazyLoadingEnabled = false;
+                    oUsuario = GetUsuarioByID(usuario.IdUsuario);
+                    if (oUsuario == null)
+                    {
+                        ctx.Usuario.Add(usuario);
+                    }
+                    else
+                    {
+                        ctx.Entry(usuario).State = System.Data.Entity.EntityState.Modified;
+                    }
+                    retorno = ctx.SaveChanges();
+                }
+                if (retorno >= 0)
+                {
+                    oUsuario = GetUsuarioByID(usuario.IdUsuario);
+                }
+
+                return oUsuario;
+            }
+            catch (DbUpdateException dbEx)
+            {
+                string mensaje = "";
+                Log.Error(dbEx, System.Reflection.MethodBase.GetCurrentMethod(), ref mensaje);
+                throw new Exception(mensaje);
+            }
+            catch (Exception ex)
+            {
+                string mensaje = "";
+                Log.Error(ex, System.Reflection.MethodBase.GetCurrentMethod(), ref mensaje);
+                throw;
+            }
+        }
+
+        public Usuario GetUsuario(string email, string password)
+        {
+            Usuario oUsuario = null;
+            try
+            {
+                using (MyContext ctx = new MyContext())
+                {
+                    ctx.Configuration.LazyLoadingEnabled = false;
+                    oUsuario = ctx.Usuario.Where(p => p.Correo.Equals(email) && p.Contra.Equals(password)).FirstOrDefault<Usuario>();
+                }
+                if (oUsuario != null)
+                {
+                    oUsuario = GetUsuarioByID(oUsuario.IdUsuario);
+                }
+
+                return oUsuario;
+            }
+            catch (DbUpdateException dbEx)
+            {
+                string mensaje = "";
+                Log.Error(dbEx, System.Reflection.MethodBase.GetCurrentMethod(), ref mensaje);
+                throw new Exception(mensaje);
+            }
+            catch (Exception ex)
+            {
+                string mensaje = "";
+                Log.Error(ex, System.Reflection.MethodBase.GetCurrentMethod(), ref mensaje);
+                throw;
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+        //
+    }
+
+}
