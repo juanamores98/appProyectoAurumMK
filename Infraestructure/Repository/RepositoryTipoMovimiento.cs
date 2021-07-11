af350fd81fc91a3e9fcbec5c9e7839a2fab5a074
SyarGekai <jeamores@est.utn.ac.cr>
2021-07-11 14:41:01 -0600

    Avance en Registro

---
+++ Infraestructure/Repository/RepositoryTipoMovimiento.cs
@@ -0,0 +1,73 @@
+﻿using Infraestructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Infraestructure.Repository
+{
+    public class RepositoryTipoMovimiento : IRepositoryTipoMovimiento
+    {
+        public IEnumerable<TipoMovimiento> GetTipoMovimiento()
+        {
+            try
+            {
+                IEnumerable<TipoMovimiento> lista = null;
+                using (MyContext ctx = new MyContext())
+                {
+                    ctx.Configuration.LazyLoadingEnabled = false;
+                    lista = ctx.TipoMovimiento
+                        .Include(x => x.RegistroMovimiento)
+                        .ToList<TipoMovimiento>();
+                }
+                return lista;
+            }
+
+            catch (DbUpdateException dbEx)
+            {
+                string mensaje = "";
+                Log.Error(dbEx, System.Reflection.MethodBase.GetCurrentMethod(), ref mensaje);
+                throw new Exception(mensaje);
+            }
+            catch (Exception ex)
+            {
+                string mensaje = "";
+                Log.Error(ex, System.Reflection.MethodBase.GetCurrentMethod(), ref mensaje);
+                throw;
+            }
+        }
+
+        public TipoMovimiento GetTipoMovimientoByID(int id)
+        {
+            try
+            {
+                TipoMovimiento oTipoMovimiento = null;
+                using (MyContext ctx = new MyContext())
+                {
+                    ctx.Configuration.LazyLoadingEnabled = false;
+                    oTipoMovimiento = ctx.TipoMovimiento
+                            .Where(p => p.IdTipoMovimiento == id)
+                            .Include(x => x.RegistroMovimiento)
+                            .FirstOrDefault();
+                }
+                return oTipoMovimiento;
+            }
+
+            catch (DbUpdateException dbEx)
+            {
+                string mensaje = "";
+                Log.Error(dbEx, System.Reflection.MethodBase.GetCurrentMethod(), ref mensaje);
+                throw new Exception(mensaje);
+            }
+            catch (Exception ex)
+            {
+                string mensaje = "";
+                Log.Error(ex, System.Reflection.MethodBase.GetCurrentMethod(), ref mensaje);
+                throw;
+            }
+        }
+    }
+}
